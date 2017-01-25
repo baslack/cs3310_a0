8a96c5f8598e8dcaedd9a33fdd38485c423183c0
Benjamin Slack <iam@nimajneb.com>
2017-01-25 11:37:31 -0500

    Testing GIT

---
+++ src/a0/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package a0;
+
+/**
+ * @author nimajneb
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World.\n");
+
+	}
+
+}
